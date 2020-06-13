17365a60a80f096ef9fcdef1a950ca6c3ef72a08
SimiS22 <simiswaminathan@gmail.com>
2020-06-13 23:45:21 +0200

    home page

---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Content.scss'
+import { Card } from 'antd'
+import developer from '../../assets/female-developer.svg'
+
+const { Meta } = Card;
+const Content: React.FC = () => {
+    return (
+        <div className='content'>
+            <img src={developer} />
+            <div className="texts">
+                <p>Hey all! I am Simi Swaminathan, Indian, living in Hamburg, Germany.</p>
+                <p>I love to build websites and apps based on HTML,CSS and Javascript.I also use React library as well as tools like NodeJS and Typescript.</p>
+                <p>Feel free to get in touch with me via below medium.</p>
+            </div>
+        </div>
+    )
+}
+export default Content

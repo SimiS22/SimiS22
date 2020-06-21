cedd041657eb28875f3405b4056084467180fc6f
SimiS22 <simiswaminathan@gmail.com>
2020-06-21 13:40:31 +0200

    modified css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Content from './components/Content/Content'
 import Contact from './components/Contact/Contact'
-import './App.css'
+import './App.scss'
 
 const App: React.FC = () => {
   return (

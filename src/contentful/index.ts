9432db869ee9e99328fd9efdb65ab406a147a35d
SimiS22 <simiswaminathan@gmail.com>
2020-07-13 12:51:41 +0200

    added works

---
+++ src/contentful/index.ts
@@ -0,0 +1,7 @@
+import * as contentful from 'contentful';
+
+export const CONTENTFUL_CLIENT = contentful.createClient({
+
+    space: 'sjqkiwsta3ek',
+    accessToken: 'P7SmtvQO-CBQJZMDYSy9SxrqVkGZHq3QOGxTiw-pSBY',
+});
--- src/pages/Works/Works.tsx
+++ src/pages/Works/Works.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { Card } from 'antd';
+import Spinner from '../../components/Spinner/Spinner'
+import { useWorks } from '../../contentful/useWorks'
+import './Works.scss'
 
+const { Meta } = Card;
 const Works: React.FC = () => {
+    const worksData = useWorks();
+    if (worksData.loading) {
+        return <Spinner />
+    }
+    const worksDataArray = worksData.works;
+
     return (
-        <div>works</div>
+        <div className='works'>{
+            worksDataArray.map((workItem, index) => {
+                return (
+                    <div key={index} className='workData'>
+                        <Card
+                            cover={<img alt={workItem.name} src={workItem.image} style={{ width: 320, objectFit: 'cover' }} />}>
+                            <Meta className="text" title={workItem.name} description={
+                                <a href={workItem.url} target="_blank" rel="noopener noreferrer">
+                                    {workItem.url}
+                                </a>
+                            } />
+                        </Card>
+                    </div>
+                )
+            })
+        }
+        </div>
     )
+
+
 }
 export default Works
